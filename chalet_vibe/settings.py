91a0c4fddeb4c0aeb6c824a728588d03e68685a2
WhiteIlya <ilyawhitee@gmail.com>
2024-09-29 13:43:16 +0200

    Deploymet db fix

--- chalet_vibe/settings.py
+++ chalet_vibe/settings.py
@@ -92,9 +92,7 @@ WSGI_APPLICATION = 'chalet_vibe.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
-    )
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 # 'default': {
